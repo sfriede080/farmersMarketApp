74ca75604a70d2941f2e1e0c7111dc3ccad5a3d3
sfriede080 <sydneyfriedel080@gmail.com>
2025-01-11 18:59:53 -0600

    fixed

--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,3 @@ app.get("/products", (req, res) => {
 app.listen(5000, () => {
     console.log('Sever started at http://localhost:5000');
 });
-
-
-// configurations for creating mysql connection
-const connection = mysql.createConnection({
-    host: 'localhost',     // host for connection
-    port: 3306,            // default port for mysql is 3306
-    database: 'farmers_market_app',      // database from which we want to connect our node application
-    user: 'root',          // username of the mysql connection
-    password: 'RunnerGirl080**'       // password of the mysql connection
-});
-
-// executing connection
-connection.connect(function(err) {
-    if (err) {
-        console.log("error occurred while connecting");
-        console.log(err);
-    } else {
-        console.log("connection created with mysql successfully");
-    }
-});
-
-connection.end();
