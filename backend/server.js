41050866a0163f7f8e522d0e6d230f5ba7e4a972
sfriede080 <sydneyfriedel080@gmail.com>
2025-01-11 18:59:24 -0600

    fixed

--- backend/server.js
+++ backend/server.js
@@ -38,21 +38,3 @@ connection.connect(function(err) {
 });
 
 connection.end();
-
-const sequelize = new Sequelize(
-    'farmers_market_app',
-    'root',
-    'RunnerGirl080**',
-    {
-        host: 'root@localhost:3306',
-        dialect: 'mysql'
-    }
-);
-
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
- }).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
- });
-
- sequelize.close();
